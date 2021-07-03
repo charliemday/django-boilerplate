d8425e61d39b4cadcfaf33cd60dbd91387a3787b
charliemday <45036245+charliemday@users.noreply.github.com>
2021-07-03 14:37:59 +0100

    Add django-extensions

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "changemeplease")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,10 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Rest Framework
+    # Third Party Apps
     "rest_framework",
     'rest_framework.authtoken',
-    # Django Apps
+    'django_extensions',
+    # Local Apps
     "users",
 ]
 
