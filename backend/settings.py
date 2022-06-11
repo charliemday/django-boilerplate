76beda37830ccf54c78095aded343f58692b6411
charliemday <45036245+charliemday@users.noreply.github.com>
2022-06-11 14:32:06 +0100

    Added whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -23,12 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "changemeplease")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-PROD = False
-if DEBUG == "False":
+DEBUG = 'PRODUCTION' not in os.environ
+if not DEBUG:
     PROD = True
 
 ALLOWED_HOSTS = []
+
+EXTERNAL_HOSTNAME = os.environ.get('EXTERNAL_HOSTNAME')
+if EXTERNAL_HOSTNAME:   
+    ALLOWED_HOSTS.append(EXTERNAL_HOSTNAME)
+
 
 # This allows us to override the User Model
 AUTH_USER_MODEL = "users.User"
@@ -52,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -155,6 +160,14 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+if not DEBUG:    
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory in production
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
